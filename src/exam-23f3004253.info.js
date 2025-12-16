30d39a5b4887c18aab20887aaa5ec77bd10b9ad1
IITM-SMRITHI <23f3004253@ds.study.iitm.ac.in>
2025-12-16 16:52:09 +0530

    Add exam configuration for TDS Bonus activity

---
+++ src/exam-23f3004253.info.js
@@ -0,0 +1,14 @@
+export default {
+  title: "TDS Bonus - exam-23f3004253",
+  start: "2025-09-01T00:00:00+05:30",
+  end: () => "2025-12-31T23:59:59+05:30",
+  allowed: () => true,
+  read: () => true,
+  admin: () => true,
+  instructions: `
+TDS Bonus Activity
+==================
+
+5 questions: Git, Docker, FastAPI, Rate Limiting, Venv
+  `
+};
